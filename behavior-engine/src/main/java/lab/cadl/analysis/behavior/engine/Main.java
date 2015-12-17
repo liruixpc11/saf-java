0962103fef87a640247dba37d3baaf81655556e4
LiRui <lirui3252007012@gmail.com>
2015-12-17 17:24:16 +0800

    init

---
+++ behavior-engine/src/main/java/lab/cadl/analysis/behavior/engine/Main.java
@@ -0,0 +1,10 @@
+package lab.cadl.analysis.behavior.engine;
+
+/**
+ *
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello, behavior negine");
+    }
+}
