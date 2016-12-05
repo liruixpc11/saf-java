2a585c3b213fecb1820eed188fa7318c7c7f77dc
Li Rui <lirui3252007012@gmail.com>
2016-12-05 22:07:23 +0800

    test file modify

--- saf-main/src/main/java/lab/cadl/analysis/behavior/saf/demo/BatchProcessor.java
+++ saf-main/src/main/java/lab/cadl/analysis/behavior/saf/demo/BatchProcessor.java
@@ -3,6 +3,8 @@ package lab.cadl.analysis.behavior.saf.demo;
 public class BatchProcessor extends DemoProcessor {
     public static void main(String[] args) {
 //        new BatchProcessor().run("data/sqlite/httpflows_1077rec.sqlite", "net.app_proto.httpdemo");
-        new BatchProcessor().run("data/sqlite/tcpudpdns_mix_20rec.sqlite", "bscripts/tcppktpair");
+//        new BatchProcessor().run("data/sqlite/tcpudpdns_mix_20rec.sqlite", "bscripts/tcppktpair");
+//        new BatchProcessor().run("data/sqlite/nsdipaper_casestudy2_data.sqlite", "bscripts/dnskaminsky_2");
+        new BatchProcessor().run("data/sqlite/nsdipaper_casestudy1_2sec_data.sqlite", "bscripts/doshussain03");
     }
 }
